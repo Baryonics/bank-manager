6f4f977ff8204a7337fbc40417487eea08889088
Baryonics <benedikt.leicher@gmail.com>
2025-07-23 17:52:29 +0200

    .

--- common/include/BaseSocket.hpp
+++ common/include/BaseSocket.hpp
@@ -1,25 +1,34 @@
 #pragma once
 #include <string>
+#include <span>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 class BaseSocket 
 {
   public:
-    /** Calls socket
-     *
-     */
-    BaseSocket(std::string path, bool is_listening);
+    BaseSocket(std::string path);
 
-    /** Calls close
-     *
-     */
     ~BaseSocket();
     int createSocket();
 
-    int recieve();
+    int recieve(std::span<const std::byte> buff);
 
-    int send();
+    int send(std::span<std::byte> buff);
+
 
   private:
     std::string path_;
     int fd_;
+    struct sockaddr_un sa_ = {};
+
+
+  protected:
+    int bind();
+
+    int listen();
+
+    int accept();
+
+    int close();
 };
--- common/src/BaseSocket.cpp
+++ common/src/BaseSocket.cpp
@@ -2,14 +2,23 @@
 #include <sys/socket.h>
 #include <sys/unistd.h>
 #include <cstring>
+#include <stdexcept>
 
-BaseSocket::BaseSocket(std::string path): path_(path)
-{
-  fd_ = socket(AF_UNIX, SOCK_STREAM, 0);
+BaseSocket::BaseSocket(std::string path) : path_(std::move(path)) {
+    fd_ = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (fd_ < 0) {
+        throw std::runtime_error("Socket creation failed");
+    }
+
+    sa_.sun_family = AF_UNIX;
+    if (path_.size() >= sizeof(sa_.sun_path)) {
+        throw std::runtime_error("Socket path too long");
+    }
+
+    std::strncpy(sa_.sun_path, path_.c_str(), sizeof(sa_.sun_path) - 1);
 }
-
 
 BaseSocket::~BaseSocket()
 {
-  close(fd_);
+  close(fd_); // ... Not done
 }
