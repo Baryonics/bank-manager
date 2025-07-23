9dc615d01e2b4ccc263b62fba8a95d84668bbfc2
Baryonics <benedikt.leicher@gmail.com>
2025-07-24 00:19:48 +0200

    added cmake for tests

--- tests/test_base_socket.cpp
+++ tests/test_base_socket.cpp
@@ -1,6 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include "BaseSocket.hpp"
 
+// what to test? dont know...
 TEST_CASE("BaseSocket creates valid socket", "[BaseSocket]") {
     BaseSocket socket("/tmp/test_socket.sock");
     REQUIRE(socket.get_fd() >= 0);
